432653ba300847bc06e9e1c512ebe7d0f4fc11de
Hardik <hardik040@yahoo.com>
2019-08-21 15:17:23 +0530

    2D Rotation

---
+++ Assets/Scripts/2D Rotation/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour
+    {
+        private Rigidbody2D _rb2D;
+
+        void Start()
+        {
+            _rb2D = GetComponent<Rigidbody2D>();
+            //_rb2D.rotation = 45f;
+        }
+
+        void FixedUpdate()
+        {
+            _rb2D.rotation += 1.0f;
+        }
+    }
---
+++ Assets/Scripts/Addressable/Address.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Addressable
+{
+    public class Address : MonoBehaviour
+    {
+        public AssetReference Ship;
+        private GameObject _s;
+        [SerializeField] private Transform pos;
+
+        private void Start()
+        {
+            Ship.LoadAssetAsync<GameObject>().Completed += OnLoadDone;
+            if (_s != null)
+            {
+                Instantiate(_s, pos.position, pos.rotation);
+            }
+            else
+            {
+                Debug.Log("Null");
+            }
+        }
+
+
+        private void OnLoadDone(AsyncOperationHandle<GameObject> obj)
+        {
+            Debug.Log(obj.Status);
+            _s = obj.Result;
+            //Instantiate(obj.Result, pos.position, pos.rotation);
+        }
+
+    }
+}
+
+
+
